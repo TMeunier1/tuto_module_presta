3bf16131e7af16d2fc08c0c60e1a4cab6c9fec86
Tibau <xiliar1888@hotmail.fr>
2017-08-02 12:08:55 +0200

    split the classes controllers in two functions

--- modules/blog/controllers/front/detail.php
+++ modules/blog/controllers/front/detail.php
@@ -1,19 +1,22 @@
 <?php
 class blogdetailModuleFrontController extends ModuleFrontController
 {
+
     public function initContent()
     {
         parent::initContent();
         $this->setTemplate('detail.tpl');
-        $post_id = Tools::getValue("id");
+        $this->context->smarty->assign("post", $this->get_post(Tools::getValue("id")));
+    }
+
+    public function get_post(int $id)
+    {
         $posts = new DbQuery();
         $posts->select('*');
         $posts->from('post');
-        $posts->where('id_post ='.$post_id);
-        $result =  Db::getInstance()->executeS($posts);
-        if ($result) {
-            $this->context->smarty->assign("post", $result);
-        }
+        $posts->where('id_post ='.$id);
+        return Db::getInstance()->executeS($posts);
+
     }
 
 }
--- modules/blog/controllers/front/list.php
+++ modules/blog/controllers/front/list.php
@@ -1,24 +1,29 @@
 <?php
 class bloglistModuleFrontController extends ModuleFrontController
 {
+
     public function initContent()
     {
         parent::initContent();
         $this->setTemplate('list.tpl');
+        $result = $this->get_posts();
+        foreach ($result as $post) {
+            $post["link"] = $this->context->link->getModuleLink('blog', 'detail',
+            array(
+                'id' => $post["id_post"]
+            ));
+            $blog_post[] = $post;
+        }
+        $this->context->smarty->assign("posts", $result);
+
+    }
+
+    public function get_posts()
+    {
         $posts = new DbQuery();
         $posts->select('*');
         $posts->from('post');
-        $result =  Db::getInstance()->executeS($posts);
-        if ($result) {
-            foreach ($result as $post) {
-                $post["link"] = $this->context->link->getModuleLink('blog', 'detail',
-                array(
-                    'id' => $post["id_post"]
-                ));
-                $blog_post[] = $post;
-            }
-            $this->context->smarty->assign("posts", $blog_post);
-        }
+        return Db::getInstance()->executeS($posts);
     }
 
 }
